d0efe091c68f96681550b160e13f98841ed9c806
Quuaann <rjck024vipro@gmail.com>
2026-02-03 16:50:07 +0700

    new

---
+++ PageObjects/Railway/HomePage.java
@@ -0,0 +1,18 @@
+package Railway;
+
+import Constant.Constant;
+
+public class HomePage extends GeneralPage {
+
+	//Loactors
+
+	//Elements
+	
+	//Methods
+	
+	public HomePage open() {
+		
+		Constant.WEBDRIVER.navigate().to(Constant.RAILWAY_URL);
+		return this;
+	}	
+}
